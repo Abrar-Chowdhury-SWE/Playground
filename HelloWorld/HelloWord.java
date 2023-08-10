645dcbd0cc2115df8fc7c992c4d22c8095d29ff5
Abrar Chowdhury <abrarchowdhury21@gmail.com>
2023-08-10 13:20:31 -0400

    got bored

---
+++ HelloWorld/HelloWord.java
@@ -0,0 +1,14 @@
+package HelloWorld;
+
+    public class HelloWord {
+        public static void main(String args[]) {
+
+            System.out.println("Hello, World!");
+                                
+        }
+    }
+
+
+
+
+
